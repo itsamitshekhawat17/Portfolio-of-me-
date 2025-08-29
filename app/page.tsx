41c92a6bb336724100f0a1958f04bfa6ed53507c
itsamitshekhawat17 <itsmeamitsingh17@gmail.com>
2025-08-29 09:27:10 +0530

    Add comment to force clean rebuild

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import { EducationExperience } from "@/components/main/education-experience";
 import { Contact } from "@/components/main/contact";
 
 export default function Home() {
+  // Main page component
   return (
     <main className="h-full w-full">
       <div className="flex flex-col gap-20">
